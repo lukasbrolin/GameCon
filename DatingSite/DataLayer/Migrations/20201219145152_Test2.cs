57cad73d40ce1e5b45b93b0d0d0f1de77109661e
lukasbrolin <lukas.brolin@live.se>
2020-12-19 15:56:12 +0100

    Update

---
+++ DatingSite/DataLayer/Migrations/20201219145152_Test2.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataLayer.Migrations
+{
+    public partial class Test2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable("Game");
+            migrationBuilder.DropTable("Genre");
+            migrationBuilder.DropTable("Personality");
+            migrationBuilder.DropTable("Platform");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
