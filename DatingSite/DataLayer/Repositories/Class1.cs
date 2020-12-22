ad01fc5b15726078b76784814e0110a3829f3830
lukasbrolin <lukas.brolin@live.se>
2020-12-22 23:03:40 +0100

    Update

---
+++ DatingSite/DataLayer/Repositories/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataLayer.Repositories
+{
+    class UserRepository : IDisposable
+    {
+        public DatingSiteContext _context { get; set; }
+
+        public UserRepository(DatingSiteContext context)
+        {
+            this._context = context;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
