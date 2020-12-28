3201d8323a7cb8354d471fa0192425ffe0233079
lukasbrolin <lukas.brolin@live.se>
2020-12-28 14:14:45 +0100

    Update

---
+++ DatingSite/DatingSite/Models/Class.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataLayer.Models;
+
+namespace DatingSite.Models
+{
+    public class ProfileViewModel
+    {
+        public User User { get; set; }
+        public IList<User> Friends { get; set; }
+        public IList<Post> Posts { get; set; }
+        public IList<User> Visitors { get; set; }
+        public IList<Game> Games { get; set; }
+        public IList<Genre> Genres { get; set; }
+        public IList<Platform> Platforms { get; set; }
+        public bool IsMatch { get; set; }
+        public int IsFriend { get; set; }
+    }
+}
