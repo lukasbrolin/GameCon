1bb0141bca3aeb0f402245d20f74a69c86af7ddf
pandrum <simon.bernsdorff@outlook.com>
2021-01-10 20:06:28 +0100

    Phet
    Notis om friends ligger nu kvar.
    Sök har samma utseende som friends.
    Games i profile är nu enhetlig med resten.
    Tabeller responsiva.
    Login nu enhetligt med resten.

--- DatingSite/DatingSite/wwwroot/js/site.js
+++ DatingSite/DatingSite/wwwroot/js/site.js
@@ -3,15 +3,15 @@
 $(function () {
     const connection = new signalR.HubConnectionBuilder().withUrl("/friendHub").build();
     connection.start().catch(err => console.error(err));
+   
     connection.on("added", (user) => {
-        console.log(user);
-    });
-
-    connection.on("added", (user) => {
-        $("#notifications").html("Added by user: <strong>" + user + "</strong>");
-        window.setTimeout(function () {
-            $("#notifications").html("");
-        },
-            5000);
+        var notifications = document.getElementById('notifications')
+        notifications.innerHTML = `<div class="alert alert-success alert-dismissible fade show m-0" role="alert">
+                          Added by user: <strong>${user}</strong>
+                          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>`
+        notifications.appendChild(div);
     });
 });
