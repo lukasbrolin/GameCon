b0d64dad8e57d77937000d51c2cbaab0838e56d1
lukasbrolin <lukas.brolin@live.se>
2021-01-11 10:19:24 +0100

    Update

--- DatingSite/DatingSite/Controllers/UsersController.cs
+++ DatingSite/DatingSite/Controllers/UsersController.cs
@@ -30,10 +30,10 @@ namespace DatingSite.Controllers
             _userRepository = new UserRepository(_context);
         }
 
-        public UsersController(DatingSiteContext context)
-        {
-            _context = context;
-        }
+        //public UsersController(DatingSiteContext context)
+        //{
+        //    _context = context;
+        //}
 
         public async Task<IActionResult> Index()
         {
